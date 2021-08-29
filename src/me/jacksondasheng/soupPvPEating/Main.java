83e96e37cf457bcfa349fab03db890fda23a9222
Jackson <71522825+Jackson-eng@users.noreply.github.com>
2021-08-28 18:35:42 -0700

    Create Main.java

---
+++ src/me/jacksondasheng/soupPvPEating/Main.java
@@ -0,0 +1,23 @@
+package me.jacksondasheng.soupPvPEating;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    public static Server server = Bukkit.getServer();
+
+    @Override
+    public void onEnable() {
+        server.getConsoleSender().sendMessage("Enabling SoupPvPEating");
+
+        server.getPluginManager().registerEvents(new SoupEater(), this);
+
+        server.getConsoleSender().sendMessage("SoupPvPEating enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        server.getConsoleSender().sendMessage("SoupPvPEating disabled");
+    }
+}
