d145fe6077de3d5ac07eff651a78f5178be2fd9c
Jackson <71522825+Jackson-eng@users.noreply.github.com>
2021-08-29 01:12:09 -0700

    Update Main.java

--- src/me/jacksondasheng/soupPvPEating/Main.java
+++ src/me/jacksondasheng/soupPvPEating/Main.java
@@ -5,7 +5,7 @@ import org.bukkit.Server;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-    public static Server server = Bukkit.getServer();
+    private static Server server = Bukkit.getServer();
 
     @Override
     public void onEnable() {
